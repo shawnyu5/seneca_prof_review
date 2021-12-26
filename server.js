cf6affbc0d54f62485fd39aaeb2d3ca0aff447c4
shawn yu <shawn.yu5@hotmail.com>
2021-12-26 18:07:23 -0500

    feat: basic routes set up

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 let mongoose = require("mongoose")
 let express = require("express")
+let handleBars = require("express-handlebars")
 
 let app = express();
-let HTTP_PORT = process.env.port || 8080;
+const HTTP_PORT = process.env.port || 8080;
 
 app.use(express.urlencoded({ extended: true }));
+
+app.engine(
+    ".hbs",
+    handleBars.engine({
+        extname: ".hbs",
+        defaultLayout: "main",
+        helpers: {}
+    })
+);
+
+app.set("view engine", ".hbs");
 
 function onStart() {
     console.log("express listening on", HTTP_PORT);
 }
+
+
+app.get("/", function(request, response) {
+    response.redirect("/profs");
+});
+
+// display all profs
+app.get("/profs", function(request, response) {
+    response.render("profs");
+});
+
+// display all courses
+app.get("/courses", function(request, response) {
+    response.render("courses");
+});
+
+// return the reviews for a spesfic course
+app.get("/course/:courseName", function(request, response) {
+    response.render("single_course");
+});
+
+// return reviews for a spesfic prof
+app.get("/prof/:name", function(request, response) {
+    response.render("single_prof");
+});
+
+// return all reviews
+app.get("/reviews", function(request, response) {
+    response.render("reviews");
+});
 
 app.listen(HTTP_PORT, onStart);
