ad10841834c7b9ed04302ecf2e7ce90db74e43f5
Shawn Yu <shawn.yu5@hotmail.com>
2022-01-06 12:09:58 -0500

    data-service errors fixed

--- data-service.js
+++ data-service.js
@@ -19,80 +19,77 @@ let sequelize = new Sequelize(
 );
 
 let Profs = sequelize.define("Profs", {
-    name: {
-        type: Sequelize.STRING,
-        primaryKey: true
-    },
-    subjects: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  subjects: Sequelize.STRING,
 });
 
 let Reviews = sequelize.define("Reviews", {
-    title: Sequelize.STRING,
-    rating: Sequelize.STRING,
-    review: Sequelize.STRING
+  title: Sequelize.STRING,
+  rating: Sequelize.STRING,
+  review: Sequelize.STRING,
 });
 
 Profs.hasMany(Reviews, {
-   foreignkey: "profName"
+  foreignkey: "profName",
 });
 
 Reviews.belongsTo(Profs);
 
 // syncs database
-module.exports.initialize = function() {
-    return new Promise(function(resolve, reject) {
-       // sequelize.drop()
-        sequelize.sync()
-            .then(function() {
-                resolve();
-            });
-    });
-}
-
-module.exports.getAllProfs = function() {
-    return new Promise(function(resolve, reject) {
-        Profs.findAll()
-            .then(function(data) {
-                resolve(data);
-            })
-            .catch((error) => {
-                console.log(error)
-                reject("Unable to retrieve all profs");
-            })
+module.exports.initialize = function () {
+  return new Promise(function (resolve, reject) {
+    // sequelize.drop()
+    sequelize.sync().then(function () {
+      resolve();
     });
   });
 };
 
-module.exports.findProf = function(name) {
-    return new Promise(function(resolve, reject) {
-        Profs.findAll({
-            profName: name
-        })
-          .then((data) => {
-                resolve(data);
-            })
-            .catch(() => {
-                reject(`No prof with ${name} found`);
-            })
-    });
-}
-
-module.exports.addReview = function(review) {
-    return new Promise(function(resolve, reject) {
-       console.log("name is " + review.profName)
-        Reviews.create({
-            title: review.reviewTitle,
-            rating: review.reviewRating,
-            review: review.reviewText,
-            // profName: review.profName
-        })
-            .then(() => {
-                resolve();
-            })
-            .catch((error) => {
-                console.log(error)
-                reject(error);
-            });
-    });
+module.exports.getAllProfs = function () {
+  return new Promise(function (resolve, reject) {
+    Profs.findAll()
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject("Unable to retrieve all profs");
+      });
+  });
 };
 
+module.exports.findProf = function (name) {
+  return new Promise(function (resolve, reject) {
+    Profs.findAll({
+      profName: name,
+    })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch(() => {
+        reject(`No prof with ${name} found`);
+      });
+  });
+};
+
+module.exports.addReview = function (review) {
+  return new Promise(function (resolve, reject) {
+    console.log("name is " + review.profName);
+    Reviews.create({
+      title: review.reviewTitle,
+      rating: review.reviewRating,
+      review: review.reviewText,
+      // profName: review.profName
+    })
+      .then(() => {
+        resolve();
+      })
+      .catch((error) => {
+        console.log(error);
+        reject(error);
+      });
+  });
+};
