dfb8886933e85389bea383bdb52ee6a2843a7102
Shawn Yu <55592708+shawnyu5@users.noreply.github.com>
2022-01-06 12:05:03 -0500

    feat: nav bar (#8)
    crud nav bar created

--- data-service.js
+++ data-service.js
@@ -1,53 +1,53 @@
-require("dotenv").config()
-const Sequelize = require('sequelize');
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
 // var sequelize = new Sequelize('database', 'user', 'password',
 let sequelize = new Sequelize(
-    process.env.DB_Database,
-    process.env.DB_User,
-    process.env.DB_Password,
-    {
-        host: 'localhost',
-        dialect: 'postgres',
-        port: process.env.DB_Port,
-        dialectOptions: {
-            ssl: { rejectUnauthorized: false }
-        },
-        query: { raw: true },
-        logging: false
-    });
+  process.env.DB_Database,
+  process.env.DB_User,
+  process.env.DB_Password,
+  {
+    host: "localhost",
+    dialect: "postgres",
+    port: process.env.DB_Port,
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
+    query: { raw: true },
+    logging: false,
+  }
+);
 
 let Profs = sequelize.define("Profs", {
-    profName: {
-        type: Sequelize.STRING,
-        primaryKey: true
-    }
+  profName: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
 });
 
 let Reviews = sequelize.define("Reviews", {
-    rating: Sequelize.STRING,
-    review: Sequelize.STRING
+  rating: Sequelize.STRING,
+  review: Sequelize.STRING,
 });
 
 // syncs database
-module.exports.initialize = function() {
-    return new Promise(function(resolve, reject) {
-        sequelize.sync()
-            .then(function() {
-                resolve();
-            });
+module.exports.initialize = function () {
+  return new Promise(function (resolve, reject) {
+    sequelize.sync().then(function () {
+      resolve();
     });
-}
+  });
+};
 
-module.exports.getAllProfs = function() {
-    return new Promise(function(resolve, reject) {
-        Profs.findAll()
-            .then(function(data) {
-                resolve(data);
-            })
-            .catch(() => {
-                reject("Unable to retrieve all profs");
-            })
-    });
-}
-
+module.exports.getAllProfs = function () {
+  return new Promise(function (resolve, reject) {
+    Profs.findAll()
+      .then(function (data) {
+        resolve(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        reject("Unable to retrieve all profs");
+      });
+  });
+};
